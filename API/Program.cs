191ffb88ad459da4c3be2361edd19a929209efdc
plamenyovchev <plamenkooo@gmail.com>
2020-01-07 20:45:26 +0200

    Use Seeder helper to seed data on startup

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -39,6 +35,7 @@ namespace API
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    Seeder.SeedData(context);
                 }
                 catch (Exception ex)
                 {
