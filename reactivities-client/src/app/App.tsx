a08457232cb7cbc3316f86a23510ddeb05cca241
plamenyovchev <plamenkooo@gmail.com>
2020-01-18 11:42:47 +0200

    Add images and move App to app folder

--- reactivities-client/src/app/App.tsx
+++ reactivities-client/src/app/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navigation from "../components/Navigation/Navigation";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Reactivities</h1>
+      <Navigation />
     </div>
   );
 };
