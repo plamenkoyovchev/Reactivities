d159e6ba8055fc523ab216d0f9dda38855db2420
plamenyovchev <plamenkooo@gmail.com>
2020-01-18 13:21:14 +0200

    refactoring

--- reactivities-client/src/components/Activities/Details/ActivityDetails.tsx
+++ reactivities-client/src/components/Activities/Details/ActivityDetails.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Image, Icon, Button } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 
 const ActivityDetails = () => {
   return (
