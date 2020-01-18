e49be911293de3ebd909fd7eaadecbc5baad2366
plamenyovchev <plamenkooo@gmail.com>
2020-01-18 11:53:58 +0200

    Load acitivites on home page initial commit

--- reactivities-client/src/app/App.tsx
+++ reactivities-client/src/app/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 import Navigation from "../components/Navigation/Navigation";
 
+import axios from "axios";
+import { Container, List } from "semantic-ui-react";
+
 const App: React.FC = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/activities")
+      .then(response => setActivities(response.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="App">
       <Navigation />
+      <Container className="Container">
+        <List>
+          {activities.map(({ id, title }) => (
+            <List.Item key={id}>{title}</List.Item>
+          ))}
+        </List>
+      </Container>
     </div>
   );
 };
