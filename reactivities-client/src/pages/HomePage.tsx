1e980884004a3a7c4209de237b2d0f8093195422
plamenyovchev <plamenkooo@gmail.com>
2020-02-01 18:45:00 +0200

    Add link to home page which navigates to /activities

--- reactivities-client/src/pages/HomePage.tsx
+++ reactivities-client/src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <Container>
       <h1>Home Page</h1>
+      <h3>
+        Go to <Link to="/activities">Activities</Link>
+      </h3>
     </Container>
   );
 }
