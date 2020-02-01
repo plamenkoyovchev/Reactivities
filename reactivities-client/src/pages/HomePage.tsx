c8473f5777293173c50c9b11d0817fe17f6e89f0
plamenyovchev <plamenkooo@gmail.com>
2020-02-01 09:18:11 +0200

    Add HomePage component

---
+++ reactivities-client/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+function HomePage() {
+  return (
+    <Container>
+      <h1>Home Page</h1>
+    </Container>
+  );
+}
+
+export default HomePage;
