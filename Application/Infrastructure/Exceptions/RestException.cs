74b0d318d59cffdb9d63e17a3917107dbaac4425
plamenyovchev <plamenkooo@gmail.com>
2020-02-10 21:06:23 +0200

    Add RestException

---
+++ Application/Infrastructure/Exceptions/RestException.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net;
+
+namespace Application.Exceptions
+{
+    public class RestException : Exception
+    {
+        public RestException(HttpStatusCode statusCode, object errors)
+        {
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+
+        public HttpStatusCode StatusCode { get; }
+        public object Errors { get; }
+    }
+}
