d21647d9f5c9df7681938535334d6daaf82aea6c
Plamen Yovchev <plamenkooo@gmail.com>
2020-02-11 15:18:57 +0200

    Add NotFound page

---
+++ reactivities-client/src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Segment } from "semantic-ui-react";
+
+const NotFound = () => {
+  return (
+    <>
+      <Segment>Ooooops, we couldn't find that page!</Segment>
+    </>
+  );
+};
+
+export default NotFound;
