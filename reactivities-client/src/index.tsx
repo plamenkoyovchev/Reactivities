379b2a2580152cbfb46b8e380e65f0b694e198c8
Plamen Yovchev <plamenkooo@gmail.com>
2020-02-12 17:40:25 +0200

    Add react toastify css

--- reactivities-client/src/index.tsx
+++ reactivities-client/src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "react-toastify/dist/ReactToastify.min.css";
+
 import "./index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
