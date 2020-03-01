6a7ff34a714a537a4fa466e7a64a1dcd62119479
plamenyovchev <plamenkooo@gmail.com>
2020-03-01 21:42:31 +0200

    Add sidebar layout

--- reactivities-client/src/components/Activities/Details/ActivityDetailedSidebar.tsx
+++ reactivities-client/src/components/Activities/Details/ActivityDetailedSidebar.tsx
@@ -1,10 +1,60 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Segment, List, Item, Label, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const ActivityDetailedSidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    <Fragment>
+      <Segment
+        textAlign="center"
+        style={{ border: "none" }}
+        attached="top"
+        secondary
+        inverted
+        color="teal"
+      >
+        3 People Going
+      </Segment>
+      <Segment attached>
+        <List relaxed divided>
+          <Item style={{ position: "relative" }}>
+            <Label
+              style={{ position: "absolute" }}
+              color="orange"
+              ribbon="right"
+            >
+              Host
+            </Label>
+            <Image size="tiny" src={"/assets/user.png"} />
+            <Item.Content verticalAlign="middle">
+              <Item.Header as="h3">
+                <Link to={`#`}>Bob</Link>
+              </Item.Header>
+              <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Item style={{ position: "relative" }}>
+            <Image size="tiny" src={"/assets/user.png"} />
+            <Item.Content verticalAlign="middle">
+              <Item.Header as="h3">
+                <Link to={`#`}>Tom</Link>
+              </Item.Header>
+              <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Item style={{ position: "relative" }}>
+            <Image size="tiny" src={"/assets/user.png"} />
+            <Item.Content verticalAlign="middle">
+              <Item.Header as="h3">
+                <Link to={`#`}>Sally</Link>
+              </Item.Header>
+            </Item.Content>
+          </Item>
+        </List>
+      </Segment>
+    </Fragment>
   );
 };
 
