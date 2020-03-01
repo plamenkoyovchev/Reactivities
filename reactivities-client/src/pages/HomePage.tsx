2370d26d5ecb1f16c77773ab5eec6feb1403f98f
plamenyovchev <plamenkooo@gmail.com>
2020-03-01 21:59:52 +0200

    Add HomePage layout and styles

--- reactivities-client/src/pages/HomePage.tsx
+++ reactivities-client/src/pages/HomePage.tsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+
+import "./HomePage.scss";
+
+import { Container, Segment, Header, Button, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <Container>
-      <h1>Home Page</h1>
-      <h3>
-        Go to <Link to="/activities">Activities</Link>
-      </h3>
-    </Container>
+    <Segment inverted textAlign="center" vertical className="masthead">
+      <Container text>
+        <Header as="h1" inverted>
+          <Image
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: 12 }}
+          />
+          Reactivities
+        </Header>
+        <Header as="h2" inverted content="Welcome to Reactivities" />
+        <Button as={Link} to="/activities" size="huge" inverted>
+          Take me to the activities!
+        </Button>
+      </Container>
+    </Segment>
   );
 }
 
