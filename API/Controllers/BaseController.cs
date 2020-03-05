711f11b85656cb30198f7e3750d11f96c507820d
plamenyovchev <plamenkooo@gmail.com>
2020-03-05 07:32:26 +0200

    Fix typo

--- API/Controllers/BaseController.cs
+++ API/Controllers/BaseController.cs
@@ -11,6 +11,6 @@ namespace API.Controllers
         private IMediator mediator;
 
         protected IMediator Mediator =>
-            mediator ?? HttpContext.RequestServices.GetService<IMediator>();
+            mediator ?? (mediator = HttpContext.RequestServices.GetService<IMediator>());
     }
 }
