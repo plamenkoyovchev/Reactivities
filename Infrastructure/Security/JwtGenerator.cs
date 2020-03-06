8a34260fb4e4f05042dd431a35d9b5e763d2f7b5
plamenyovchev <plamenkooo@gmail.com>
2020-03-06 19:36:17 +0200

    Implement JwtToken Generation

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Application.Common.Interfaces;
 using Domain;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
@@ -7,7 +13,42 @@ namespace Infrastructure.Security
     {
         public string CreateToken(ReactivityUser user)
         {
-            return string.Empty;
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var signingCredentials = CreateSigningCredentials();
+            var tokenDescriptor = CreateTokenDescriptor(claims, signingCredentials);
+            var token = GenerateToken(tokenDescriptor);
+
+            return token;
+        }
+
+        private string GenerateToken(SecurityTokenDescriptor tokenDescriptor)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        private SigningCredentials CreateSigningCredentials()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("should be secret"));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            return credentials;
+        }
+
+        private SecurityTokenDescriptor CreateTokenDescriptor(IEnumerable<Claim> claims, SigningCredentials credentials)
+        {
+            return new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = credentials
+            };
         }
     }
 }
