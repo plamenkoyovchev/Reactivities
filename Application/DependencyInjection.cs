69e20430ac9dd6f5fffc3914092bc1b4445c4966
Plamen Yovchev <plamenkooo@gmail.com>
2020-03-06 09:18:56 +0200

    Add DepencyInjection helper to register Application dependecies

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
