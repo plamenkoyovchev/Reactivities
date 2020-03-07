f10ab25e8dd66c6930955468367e91b063532eb6
plamenyovchev <plamenkooo@gmail.com>
2020-03-07 13:39:32 +0200

    Validate Password with Password validator extension method

--- Application/Authentication/Register/RegisterCommandValidator.cs
+++ Application/Authentication/Register/RegisterCommandValidator.cs
@@ -1,3 +1,4 @@
+using Application.Common.Extensions;
 using FluentValidation;
 
 namespace Application.Authentication.Register
@@ -6,10 +7,10 @@ namespace Application.Authentication.Register
     {
         public RegisterCommandValidator()
         {
-            RuleFor(u => u.Email).EmailAddress().NotEmpty();
+            RuleFor(u => u.Email).NotEmpty().EmailAddress();
             RuleFor(u => u.Username).NotEmpty();
             RuleFor(u => u.DisplayName).NotEmpty();
-            RuleFor(u => u.Password).NotEmpty().MinimumLength(8);
+            RuleFor(u => u.Password).Password();
         }
     }
 }
