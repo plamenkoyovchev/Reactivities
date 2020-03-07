a99bb39eacf9b3edb95370efb4eba3501730c1aa
plamenyovchev <plamenkooo@gmail.com>
2020-03-07 09:06:27 +0200

    Add AllowAnonymous attr to UserController

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
 using Application.Authentication.Login;
 using Application.Common.ViewModels.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
         [HttpPost("login")]
