41e2fae77f9d8d28a4cf1606c961d18f53fdd606
plamenyovchev <plamenkooo@gmail.com>
2020-03-07 08:16:04 +0200

    Use only ErrorHandlingMiddleware for handling errors

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,14 +53,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseMiddleware<ErrorHandlingMiddleware>();
-            }
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseRouting();
             app.UseCors("CorsPolicy");
