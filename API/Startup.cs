ddde59d1d72bf2cd5240acacde1bdff10c90cf76
plamenyovchev <plamenkooo@gmail.com>
2020-03-07 07:56:48 +0200

    UseDeveloperExceptionPage in Develepment Environment

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,10 +52,13 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseMiddleware<ErrorHandlingMiddleware>();
             if (env.IsDevelopment())
             {
-                //app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseMiddleware<ErrorHandlingMiddleware>();
             }
 
             app.UseRouting();
