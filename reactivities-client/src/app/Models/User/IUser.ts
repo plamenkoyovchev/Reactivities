847df2d080e7947f36542cd9d7506ba4bfd09968
plamenyovchev <plamenkooo@gmail.com>
2020-03-08 09:20:19 +0200

    Add IUser & IUserFromValues interfaces

---
+++ reactivities-client/src/app/Models/User/IUser.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  email: string;
+  displayName: string;
+  username: string;
+  token: string;
+  image?: string;
+}
---
+++ reactivities-client/src/app/Models/User/IUserFormValues.ts
@@ -0,0 +1,6 @@
+export interface IUserFormValues {
+  email: string;
+  password: string;
+  username: string;
+  displayName: string;
+}
