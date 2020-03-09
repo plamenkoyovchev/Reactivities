994abc6f1d143e7056080261287a42c5ee5b7205
plamenyovchev <plamenkooo@gmail.com>
2020-03-09 22:47:25 +0200

    Add possibility to add text below the loading spinner

--- reactivities-client/src/components/UI/Loader/Loader.tsx
+++ reactivities-client/src/components/UI/Loader/Loader.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Loader.scss";
 
-const Loader = () => {
-  return <div className="loader">Loading...!</div>;
+interface IProps {
+  loadingText?: string;
+}
+
+const Loader: React.FC<IProps> = props => {
+  const { loadingText } = props;
+  return (
+    <>
+      <div className="loader"></div>
+      <h2 className="loader-text">{loadingText}</h2>
+    </>
+  );
 };
 
 export default Loader;
