1b0782d1c26635cf0d70af853384b2348b884442
plamenyovchev <plamenkooo@gmail.com>
2020-03-13 21:13:01 +0200

    Add ModalStore to RootStore

--- reactivities-client/src/shared/stores/rootStore.ts
+++ reactivities-client/src/shared/stores/rootStore.ts
@@ -2,16 +2,19 @@ import { createContext } from "react";
 import ActivityStore from "./activity/activityStore";
 import UserStore from "./user/userStore";
 import CommonStore from "./common/commonStore";
+import ModalStore from "./modal/modalStore";
 
 export class RootStore {
   activityStore: ActivityStore;
   userStore: UserStore;
   commonStore: CommonStore;
+  modalStore: ModalStore;
 
   constructor() {
     this.activityStore = new ActivityStore();
     this.userStore = new UserStore(this);
     this.commonStore = new CommonStore();
+    this.modalStore = new ModalStore(this);
   }
 }
 
