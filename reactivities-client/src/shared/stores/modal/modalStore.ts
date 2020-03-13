f5ff1553f89fe391fc7bf24ed11ff559e1f9d900
plamenyovchev <plamenkooo@gmail.com>
2020-03-13 21:12:48 +0200

    Add ModalStore

---
+++ reactivities-client/src/shared/stores/modal/modalStore.ts
@@ -0,0 +1,26 @@
+import { observable, action } from "mobx";
+import { RootStore } from "../rootStore";
+
+class ModalStore {
+  rootStore: RootStore;
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+  }
+
+  @observable modal = {
+    open: false,
+    body: null
+  };
+
+  @action open = (content: any) => {
+    this.modal.open = true;
+    this.modal.body = content;
+  };
+
+  @action close = () => {
+    this.modal.open = false;
+    this.modal.body = null;
+  };
+}
+
+export default ModalStore;
