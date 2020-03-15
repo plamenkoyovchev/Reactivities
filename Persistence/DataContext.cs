d0c98df37497ca0306ee6bb7cbd2e7e8a6bc9c61
Plamen Yovchev <plamenkooo@gmail.com>
2020-03-15 18:25:09 +0200

    Configure Many to many relationship between User and Activity

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -16,6 +16,8 @@ namespace Persistence
 
         public DbSet<Activity> Activities { get; set; }
 
+        public DbSet<UserActivity> UserActivities { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -27,6 +29,19 @@ namespace Persistence
                     new Value { Id = 2, Name = "Value 2" },
                     new Value { Id = 3, Name = "Value 3" }
                 });
+
+            modelBuilder.Entity<UserActivity>()
+                .HasKey(x => new { x.ReactivityUserId, x.ActivityId });
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(a => a.ReactivityUser)
+                .WithMany(ua => ua.UserActivities)
+                .HasForeignKey(r => r.ReactivityUserId);
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(u => u.Activity)
+                .WithMany(ua => ua.UserActivities)
+                .HasForeignKey(a => a.ActivityId);
         }
     }
 }
