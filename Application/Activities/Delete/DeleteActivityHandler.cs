154057cbbbd88460ea8db07d0fac1f3e83928498
Plamen Yovchev <plamenkooo@gmail.com>
2020-03-18 17:06:53 +0200

    Add check if user which delete or edit activity is host of the activity

--- Application/Activities/Delete/DeleteActivityHandler.cs
+++ Application/Activities/Delete/DeleteActivityHandler.cs
@@ -1,28 +1,49 @@
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Common;
 using Application.Common.Exceptions;
+using Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities.Delete
 {
     public class DeleteActivityHandler : HandlerBase, IRequestHandler<DeleteActivityCommand>
     {
-        public DeleteActivityHandler(DataContext context) : base(context)
+        private readonly IUserAccessor userAccessor;
+
+        public DeleteActivityHandler(DataContext context, IUserAccessor userAccessor)
+            : base(context)
         {
+            this.userAccessor = userAccessor;
         }
 
         public async Task<Unit> Handle(DeleteActivityCommand request, CancellationToken cancellationToken)
         {
-            var activity = await this.Context.Activities.FindAsync(request.Id);
+            var activity = await this.Context.Activities
+                                    .Include(a => a.UserActivities)
+                                    .FirstOrDefaultAsync(a => a.Id == request.Id);
             if (activity == null)
             {
                 throw new RestException(HttpStatusCode.NotFound, new
                 {
                     activity = "Not found"
                 });
+            }
+
+            var user = await this.Context.Users.FirstOrDefaultAsync(u => u.UserName == this.userAccessor.GetUsername());
+            if (user == null)
+            {
+                throw new RestException(HttpStatusCode.BadRequest);
+            }
+
+            var hostUser = activity.UserActivities.FirstOrDefault(ua => ua.ReactivityUserId == user.Id && ua.IsHost);
+            if (hostUser == null)
+            {
+                throw new RestException(HttpStatusCode.Forbidden, new { Activity = "Only hosts can delete activities!" });
             }
 
             this.Context.Remove(activity);
--- Application/Activities/Edit/EditActivityHandler.cs
+++ Application/Activities/Edit/EditActivityHandler.cs
@@ -1,8 +1,10 @@
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Common;
 using Application.Common.Exceptions;
+using Application.Common.Interfaces;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,19 +14,36 @@ namespace Application.Activities.Edit
 {
     public class EditActivityHandler : HandlerBase, IRequestHandler<EditActivityCommand, Activity>
     {
-        public EditActivityHandler(DataContext context) : base(context)
+        private readonly IUserAccessor userAccessor;
+
+        public EditActivityHandler(DataContext context, IUserAccessor userAccessor) : base(context)
         {
+            this.userAccessor = userAccessor;
         }
 
         public async Task<Activity> Handle(EditActivityCommand request, CancellationToken cancellationToken)
         {
-            var activityToEdit = await this.Context.Activities.FindAsync(request.Id);
+            var activityToEdit = await this.Context.Activities
+                                            .Include(a => a.UserActivities)
+                                            .FirstOrDefaultAsync(a => a.Id == request.Id);
             if (activityToEdit == null)
             {
                 throw new RestException(HttpStatusCode.NotFound, new
                 {
                     activity = "Not found"
                 });
+            }
+
+            var user = await this.Context.Users.FirstOrDefaultAsync(u => u.UserName == this.userAccessor.GetUsername());
+            if (user == null)
+            {
+                throw new RestException(HttpStatusCode.BadRequest);
+            }
+
+            var hostUser = activityToEdit.UserActivities.FirstOrDefault(ua => ua.ReactivityUserId == user.Id && ua.IsHost);
+            if (hostUser == null)
+            {
+                throw new RestException(HttpStatusCode.Forbidden, new { Activity = "Only hosts can edit activities!" });
             }
 
             activityToEdit.Category = request.Category ?? activityToEdit.Category;
