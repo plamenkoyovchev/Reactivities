1a00b7625ee42ae2528c256f3b0c71c2ef187ba9
plamenyovchev <plamenkooo@gmail.com>
2020-03-24 21:32:55 +0200

    Add CloudinarySettings

---
+++ Infrastructure/Photos/CloudinarySettings.cs
@@ -0,0 +1,11 @@
+namespace Infrastructure.Photos
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string ApiSecret { get; set; }
+    }
+}
