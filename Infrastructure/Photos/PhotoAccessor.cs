f72cc5e0760e28f3e19c48712a0758420e78eb67
plamenyovchev <plamenkooo@gmail.com>
2020-03-24 22:15:56 +0200

    PhotoAccessor initial commit

---
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,54 @@
+using Application.Common.Interfaces;
+using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor : IPhotoAccessor
+    {
+        private readonly Cloudinary cloudinary;
+
+        public PhotoAccessor(IOptions<CloudinarySettings> options)
+        {
+            var account = new Account(options.Value.CloudName, options.Value.ApiKey, options.Value.ApiSecret);
+            this.cloudinary = new Cloudinary(account);
+        }
+
+        public PhotoUploadResult AddPhoto(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+            if (file != null && file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams
+                    {
+                        File = new FileDescription(file.FileName, stream)
+                    };
+                    uploadResult = this.cloudinary.Upload(uploadParams);
+                }
+            }
+
+            return new PhotoUploadResult
+            {
+                PublicId = uploadResult.PublicId,
+                Url = uploadResult.SecureUri.AbsoluteUri
+            };
+        }
+
+        public string DeletePhoto(string publicId)
+        {
+            var result = string.Empty;
+            if (!string.IsNullOrWhiteSpace(publicId))
+            {
+                var deleteParams = new DeletionParams(publicId);
+                result = this.cloudinary.Destroy(deleteParams).Result;
+            }
+
+            return result;
+        }
+    }
+}
