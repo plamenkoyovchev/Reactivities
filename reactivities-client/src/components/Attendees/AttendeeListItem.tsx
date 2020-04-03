ed1fa2ff22e1a486d10c85ad31401d5b77a27458
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-03 22:02:29 +0300

    Link to profile

--- reactivities-client/src/components/Attendees/AttendeeListItem.tsx
+++ reactivities-client/src/components/Attendees/AttendeeListItem.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IAttendee } from "../../app/Models/Attendee/IAttendee";
 import { List, Image, Popup } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 interface IProps {
   attendee: IAttendee;
@@ -14,7 +15,9 @@ const AttendeeListItem: React.FC<IProps> = ({ attendee }) => (
         trigger={<Image avatar src={attendee.image || "/assets/user.png"} />}
       />
       <List.Content>
-        <List.Header>{attendee.username}</List.Header>
+        <List.Header>
+          <Link to={`/profile/${attendee.username}`}>{attendee.username}</Link>
+        </List.Header>
       </List.Content>
     </List.Item>
   </>
