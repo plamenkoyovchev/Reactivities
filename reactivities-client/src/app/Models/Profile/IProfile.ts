011d86e8d1a86c2653635fbba9ea3ede9ea7d54b
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-04 13:58:56 +0300

    Implement profile page header

---
+++ reactivities-client/src/app/Models/Profile/IProfile.ts
@@ -0,0 +1,4 @@
+export interface IProfile {
+  displayName: string;
+  image: string;
+}
--- reactivities-client/src/pages/ProfilePage.tsx
+++ reactivities-client/src/pages/ProfilePage.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import ProfileHeader from "../components/User/Profile/ProfileHeader";
+import { RootStoreContext } from "../shared/stores/rootStore";
+import { IProfile } from "../app/Models/Profile/IProfile";
 
 const ProfilePage = () => {
+  const rootStore = useContext(RootStoreContext);
+  const currentUser = rootStore.userStore.currentUser as IProfile;
   return (
-    <div>
-      <h1>Profile Page</h1>
-    </div>
+    <>
+      <ProfileHeader profile={currentUser} />
+    </>
   );
 };
 
