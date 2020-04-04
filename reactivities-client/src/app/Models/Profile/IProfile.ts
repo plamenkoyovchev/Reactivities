4e0e38c3eaad854bf6811bc59b45cb97cc46c567
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-04 14:19:31 +0300

    Add more properties to IProfile interface

--- reactivities-client/src/app/Models/Profile/IProfile.ts
+++ reactivities-client/src/app/Models/Profile/IProfile.ts
@@ -1,4 +1,13 @@
 export interface IProfile {
+  username: string;
   displayName: string;
+  bio: string;
   image: string;
+  photos: IPhoto[];
+}
+
+export interface IPhoto {
+  id: string;
+  url: string;
+  isMain: boolean;
 }
