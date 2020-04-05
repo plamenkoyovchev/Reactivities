2215feec73c1b56fec3bce16b5f3f1fbf2c6c1f9
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-05 12:30:40 +0300

    PhotoUploadWidget initial commit

---
+++ reactivities-client/src/components/Common/Photos/PhotoUploadWidget.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PhotoUploadWidget = () => {
+  return (
+    <div>
+      <h1>Photo upload widget</h1>
+    </div>
+  );
+};
+
+export default PhotoUploadWidget;
