5dd1cc886c3b75f5428cba7f6f2e446eff664967
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-06 20:58:46 +0300

    Add postFormData method to be able to send files to the  server

--- reactivities-client/src/shared/axios/httpRequester.ts
+++ reactivities-client/src/shared/axios/httpRequester.ts
@@ -5,7 +5,7 @@ import { history } from "../..";
 import { toast } from "react-toastify";
 import { IUser } from "../../app/Models/User/IUser";
 import { IUserFormValues } from "../../app/Models/User/IUserFormValues";
-import { IProfile } from "../../app/Models/Profile/IProfile";
+import { IProfile, IPhoto } from "../../app/Models/Profile/IProfile";
 
 const httpStatusCodes = {
   BAD_REQUEST: 400,
@@ -63,6 +63,17 @@ const request = {
   post: (url: string, data: {}) => axios.post(url, data).then(responseBody),
   put: (url: string, data: {}) => axios.put(url, data).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
+  postFormData: (url: string, file: Blob) => {
+    const formData = new FormData();
+    formData.append("File", file);
+    return axios
+      .post(url, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then(responseBody);
+  },
 };
 
 const activities = {
@@ -89,6 +100,8 @@ const user = {
 const profile = {
   get: (username: string): Promise<IProfile> =>
     request.get(`/user/${username}/profile`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> =>
+    request.postFormData("/photos", photo),
 };
 
 export default {
