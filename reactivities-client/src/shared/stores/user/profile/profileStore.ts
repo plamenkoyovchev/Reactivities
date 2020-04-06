42a5d1afa3a7264944157cb0c48a70680de17e8e
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-06 21:07:17 +0300

    Add uploadPhoto action to profileStore

--- reactivities-client/src/shared/stores/user/profile/profileStore.ts
+++ reactivities-client/src/shared/stores/user/profile/profileStore.ts
@@ -2,6 +2,7 @@ import { RootStore } from "./../../rootStore";
 import { observable, action, runInAction, computed } from "mobx";
 import { IProfile } from "../../../../app/Models/Profile/IProfile";
 import httpRequester from "../../../axios/httpRequester";
+import { toast } from "react-toastify";
 
 class ProfileStore {
   rootStore: RootStore;
@@ -12,6 +13,7 @@ class ProfileStore {
 
   @observable loading = true;
   @observable profile: IProfile | null = null;
+  @observable uploadingPhoto = false;
 
   @computed get isCurrentUser() {
     const currentUser = this.rootStore.userStore.currentUser;
@@ -34,6 +36,28 @@ class ProfileStore {
     } finally {
       runInAction(() => {
         this.loading = false;
+      });
+    }
+  };
+
+  @action uploadPhoto = async (file: Blob) => {
+    this.uploadingPhoto = true;
+    try {
+      const photo = await httpRequester.profile.uploadPhoto(file);
+      runInAction(() => {
+        if (this.profile) {
+          this.profile.photos.push(photo);
+          if (photo.isMain && this.rootStore.userStore.currentUser) {
+            this.rootStore.userStore.currentUser.image = photo.url;
+            this.profile.photo = photo;
+          }
+        }
+      });
+    } catch (error) {
+      toast.error("There was a problem while uploading your photo");
+    } finally {
+      runInAction(() => {
+        this.uploadingPhoto = false;
       });
     }
   };
