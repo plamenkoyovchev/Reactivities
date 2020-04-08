33becb163630d189948f2cc197dcd3c1930d9d56
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-08 17:13:27 +0300

    Add Comments DbSet to DataContext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -20,6 +20,8 @@ namespace Persistence
 
         public DbSet<Photo> Photos { get; set; }
 
+        public DbSet<Comment> Comments { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
