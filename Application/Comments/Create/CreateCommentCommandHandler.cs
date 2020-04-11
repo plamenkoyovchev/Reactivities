38895af31f9bd4eefa553429e4cc38301d8de5ab
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-11 10:40:07 +0300

    Create comment command handler implementation

--- Application/Comments/Create/CreateCommentCommandHandler.cs
+++ Application/Comments/Create/CreateCommentCommandHandler.cs
@@ -1,23 +1,58 @@
+using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Common;
 using Application.Common.DTOs.Comments;
+using Application.Common.Exceptions;
+using AutoMapper;
+using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Comments.Create
 {
     public class CreateCommentCommandHandler : HandlerBase, IRequestHandler<CreateCommentCommand, CommentDTO>
     {
-        public CreateCommentCommandHandler(DataContext context)
+        private readonly IMapper mapper;
+
+        public CreateCommentCommandHandler(DataContext context, IMapper mapper)
             : base(context)
         {
-
+            this.mapper = mapper;
         }
 
-        public Task<CommentDTO> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
+        public async Task<CommentDTO> Handle(CreateCommentCommand request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var activity = await Context.Activities.FindAsync(request.ActivityId);
+            if (activity == null)
+            {
+                throw new RestException(HttpStatusCode.NotFound, new { Activity = "Not found" });
+            }
+
+            var user = await Context.Users.Include(u => u.Comments).FirstOrDefaultAsync(u => u.UserName == request.Username);
+            if (user == null)
+            {
+                throw new RestException(HttpStatusCode.NotFound);
+            }
+
+            var comment = new Comment
+            {
+                Activity = activity,
+                Author = user,
+                Body = request.Body,
+                CreatedOn = DateTime.UtcNow
+            };
+
+            activity.Commets.Add(comment);
+
+            if (await Context.SaveChangesAsync() > 0)
+            {
+                return mapper.Map<CommentDTO>(comment);
+            }
+
+            throw new Exception("Problem occured while saving comment");
         }
     }
 }
