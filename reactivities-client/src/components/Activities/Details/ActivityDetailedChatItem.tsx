ada1f4876dcadc492d8204c1530c9ee38978144d
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-13 00:03:04 +0300

    Fix typos & refactoring

--- reactivities-client/src/components/Activities/Details/ActivityDetailedChatItem.tsx
+++ reactivities-client/src/components/Activities/Details/ActivityDetailedChatItem.tsx
@@ -9,22 +9,17 @@ interface IProps {
 }
 
 const ActivityDetailedChatItem: React.FC<IProps> = ({ comment }) => {
-  const { username, displayName, image, body, createdAt } = comment;
+  const { username, displayName, photo, body, createdOn } = comment;
 
   return (
     <Comment>
-      <Comment.Avatar src={`${image} || "/assets/user.png"`} />
+      <Comment.Avatar src={photo || "/assets/user.png"} />
       <Comment.Content>
         <Comment.Author as={Link} to={`/profile/${username}`}>
           {displayName}
         </Comment.Author>
-        <Comment.Metadata>
-          <div>{createdAt}</div>
-        </Comment.Metadata>
-        <Comment.Text>{body}</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
+        <Comment.Metadata>{createdOn}</Comment.Metadata>
+        <Comment.Text>{body || "Empty"}</Comment.Text>
       </Comment.Content>
     </Comment>
   );
