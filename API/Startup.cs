68a42e5896ee55db88e6effdc85c359743dbf916
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-20 14:41:04 +0300

    Register IProfileReader

--- API/Startup.cs
+++ API/Startup.cs
@@ -5,6 +5,7 @@ using Application;
 using Application.Activities.Create;
 using Application.Common.Constants.System;
 using Application.Common.Interfaces;
+using Application.UserProfile;
 using FluentValidation.AspNetCore;
 using Infrastructure.Photos;
 using Infrastructure.Security;
@@ -46,6 +47,7 @@ namespace API
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.AddScoped<IProfileReader, ProfileReader>();
 
             services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
         }
