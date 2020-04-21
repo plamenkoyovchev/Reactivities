74334a728fbbd3b363e21a5eeb61d1573da9f214
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-21 16:26:36 +0300

    Add orange border if we are following an attendee of our activity

--- reactivities-client/src/components/Attendees/AttendeeListItem.tsx
+++ reactivities-client/src/components/Attendees/AttendeeListItem.tsx
@@ -3,6 +3,8 @@ import { IAttendee } from "../../app/Models/Attendee/IAttendee";
 import { List, Image, Popup } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
+import "./AttendeeListItem.scss";
+
 interface IProps {
   attendee: IAttendee;
 }
@@ -12,7 +14,14 @@ const AttendeeListItem: React.FC<IProps> = ({ attendee }) => (
     <List.Item>
       <Popup
         content={attendee.displayName}
-        trigger={<Image avatar src={attendee.image || "/assets/user.png"} />}
+        trigger={
+          <Image
+            avatar
+            src={attendee.image || "/assets/user.png"}
+            bordered
+            className={attendee.following ? "following" : ""}
+          />
+        }
       />
       <List.Content>
         <List.Header>
