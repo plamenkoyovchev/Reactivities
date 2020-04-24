16627e3d02d848dcc48cb9e7eec1d738ca90ec9b
plamenyovchev <plamenkooo@gmail.com>
2020-04-24 23:25:55 +0300

    Refactor Seeder

--- Persistence/Seeder.cs
+++ Persistence/Seeder.cs
@@ -28,18 +28,21 @@ namespace Persistence
             {
                 new ReactivityUser()
                 {
+                    Id = "string-12",
                     DisplayName = "Pako",
                     UserName = "pakata",
                     Email = "pakata@abv.bg"
                 },
                 new ReactivityUser()
                 {
+                    Id = "string-23",
                     DisplayName = "Sako",
                     UserName = "sakata",
                     Email = "sakata@abv.bg"
                 },
                 new ReactivityUser()
                 {
+                    Id = "string-34",
                     DisplayName = "Cako",
                     UserName = "cakata",
                     Email = "cakata@abv.bg"
@@ -58,97 +61,45 @@ namespace Persistence
                 {
                     new Activity
                     {
-                        Title = "Past Activity 1",
-                        Date = DateTime.Now.AddMonths(-2),
-                        Description = "Activity 2 months ago",
+                        Title = "CQRS Demo",
+                        Date = DateTime.Now.AddDays(2),
+                        Description = "CQRS Demo",
                         Category = "drinks",
                         City = "London",
                         Venue = "Pub",
+
                     },
                     new Activity
                     {
-                        Title = "Past Activity 2",
-                        Date = DateTime.Now.AddMonths(-1),
-                        Description = "Activity 1 month ago",
+                        Title = "Develop .Net Core Apps with VSC",
+                        Date = DateTime.Now.AddDays(5),
+                        Description = "Develop .Net Core Apps with VSC",
                         Category = "culture",
                         City = "Paris",
                         Venue = "Louvre",
                     },
                     new Activity
                     {
-                        Title = "Future Activity 1",
+                        Title = "React & MobX",
                         Date = DateTime.Now.AddMonths(1),
-                        Description = "Activity 1 month in future",
+                        Description = "React & MobX",
                         Category = "culture",
                         City = "London",
                         Venue = "Natural History Museum",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 2",
-                        Date = DateTime.Now.AddMonths(2),
-                        Description = "Activity 2 months in future",
-                        Category = "music",
-                        City = "London",
-                        Venue = "O2 Arena",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 3",
-                        Date = DateTime.Now.AddMonths(3),
-                        Description = "Activity 3 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Another pub",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 4",
-                        Date = DateTime.Now.AddMonths(4),
-                        Description = "Activity 4 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Yet another pub",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 5",
-                        Date = DateTime.Now.AddMonths(5),
-                        Description = "Activity 5 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Just another pub",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 6",
-                        Date = DateTime.Now.AddMonths(6),
-                        Description = "Activity 6 months in future",
-                        Category = "music",
-                        City = "London",
-                        Venue = "Roundhouse Camden",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 7",
-                        Date = DateTime.Now.AddMonths(7),
-                        Description = "Activity 2 months ago",
-                        Category = "travel",
-                        City = "London",
-                        Venue = "Somewhere on the Thames",
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 8",
-                        Date = DateTime.Now.AddMonths(8),
-                        Description = "Activity 8 months in future",
-                        Category = "film",
-                        City = "London",
-                        Venue = "Cinema",
                     }
                 };
 
-            context.Activities.AddRange(activities);
+            foreach (var activity in activities)
+            {
+                var userActivity = new UserActivity()
+                {
+                    ReactivityUserId = "string-12",
+                    Activity = activity,
+                    IsHost = true
+                };
+                context.UserActivities.Add(userActivity);
+            }
+
             context.SaveChanges();
         }
     }
