2efe53e61db2d9e739db22ed9240dcc6850b92f7
Plamen Yovchev <plamenkooo@gmail.com>
2020-04-25 22:40:17 +0300

    Add margin-top to error message label

---
+++ reactivities-client/src/components/UI/Form/TextAreaInput.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./TextAreaInput.scss";
+
+import { FieldRenderProps } from "react-final-form";
+import { FormFieldProps, Form, Label } from "semantic-ui-react";
+
+interface IProps
+  extends FieldRenderProps<string, HTMLElement>,
+    FormFieldProps {}
+
+const TextAreaInput: React.FC<IProps> = ({
+  input,
+  width,
+  rows,
+  placeholder,
+  meta: { touched, error },
+}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <textarea rows={rows} {...input} placeholder={placeholder} />
+      {touched && error && (
+        <Label className="error-message" basic color="red">
+          {error}
+        </Label>
+      )}
+    </Form.Field>
+  );
+};
+
+export default TextAreaInput;
