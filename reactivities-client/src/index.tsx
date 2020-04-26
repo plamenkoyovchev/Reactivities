30d6f3a28a1142db8cc89ca05f49237078e80437
plamenyovchev <plamenkooo@gmail.com>
2020-04-26 13:13:54 +0300

    Import react-widgets styles

--- reactivities-client/src/index.tsx
+++ reactivities-client/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "react-toastify/dist/ReactToastify.min.css";
-
+import "react-widgets/dist/css/react-widgets.css";
 import "./index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
