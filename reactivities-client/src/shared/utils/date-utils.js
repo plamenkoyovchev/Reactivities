a99afd10e67163beb31748c58c9d35eaec765bb7
plamenyovchev <plamenkooo@gmail.com>
2020-04-26 15:11:43 +0300

    Add combineDateAndTime

--- reactivities-client/src/shared/utils/date-utils.js
+++ reactivities-client/src/shared/utils/date-utils.js
@@ -20,3 +20,14 @@ export const formatAsDistance = (date) => {
 
   return formatDistance(new Date(date), new Date());
 }
+
+export const combineDateAndTime = (date, time) => {
+  const timeString = `${time.getHours()}:${time.getMinutes()}:00`;
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const dateString = `${year}-${month}-${day}`;
+
+  return new Date(`${dateString} ${timeString}`);
+}
