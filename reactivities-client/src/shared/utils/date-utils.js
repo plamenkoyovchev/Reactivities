078f1bf6b36c60500d395cd84ffe5e53f09bf36d
plamenyovchev <plamenkooo@gmail.com>
2020-04-26 22:38:09 +0300

    Refactor combineDateAndTime function

--- reactivities-client/src/shared/utils/date-utils.js
+++ reactivities-client/src/shared/utils/date-utils.js
@@ -22,12 +22,12 @@ export const formatAsDistance = (date) => {
 }
 
 export const combineDateAndTime = (date, time) => {
-  const timeString = `${time.getHours()}:${time.getMinutes()}:00`;
+  const timeString = `${time.getHours()}:${time.getMinutes()}:00+0000`;
 
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const dateString = `${year}-${month}-${day}`;
 
-  return new Date(`${dateString} ${timeString}`);
+  return new Date(Date.parse(`${dateString} ${timeString}`));
 }
