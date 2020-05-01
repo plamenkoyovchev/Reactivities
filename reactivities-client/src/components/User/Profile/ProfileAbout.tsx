2079b940ef4a40b2e907249037ca6e31fab5f00c
plamenyovchev <plamenkooo@gmail.com>
2020-05-01 16:31:49 +0300

    Load date into ProfileAbout component

--- reactivities-client/src/components/User/Profile/ProfileAbout.tsx
+++ reactivities-client/src/components/User/Profile/ProfileAbout.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RootStoreContext } from "../../../shared/stores/rootStore";
 
 const ProfileAbout = () => {
-  return <div>About</div>;
+  const rootStore = useContext(RootStoreContext);
+  const { profileStore } = rootStore;
+  const { profile } = profileStore;
+
+  if (!profile) {
+    return null;
+  }
+
+  return <div>{profile.bio}</div>;
 };
 
 export default ProfileAbout;
