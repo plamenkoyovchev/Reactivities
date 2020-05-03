c4cb66134fd1166e6ad15f43e10f411a17d4d641
plamenyovchev <plamenkooo@gmail.com>
2020-05-03 12:15:55 +0300

    Implement ActivityListItemPlaceholder

--- reactivities-client/src/components/Activities/List/ListItem/ActivityListItemPlaceholder.tsx
+++ reactivities-client/src/components/Activities/List/ListItem/ActivityListItemPlaceholder.tsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import { Segment, Button, Placeholder } from "semantic-ui-react";
 const ActivityListItemPlaceholder = () => {
-  return <div></div>;
+  return (
+    <>
+      <Placeholder fluid style={{ marginTop: 50 }}>
+        <Segment.Group>
+          <Segment style={{ minHeight: 110 }}>
+            <Placeholder>
+              <Placeholder.Header image>
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Header>
+              <Placeholder.Paragraph>
+                <Placeholder.Line />
+              </Placeholder.Paragraph>
+            </Placeholder>
+          </Segment>
+          <Segment>
+            <Placeholder>
+              <Placeholder.Line />
+              <Placeholder.Line />
+            </Placeholder>
+          </Segment>
+          <Segment secondary style={{ minHeight: 70 }} />
+          <Segment clearing>
+            <Button disabled color="blue" floated="right" content="View" />
+          </Segment>
+        </Segment.Group>
+      </Placeholder>
+    </>
+  );
 };
 
 export default ActivityListItemPlaceholder;
