7ebe756bfeb5e50acbb7963967604608b78b6ac4
Plamen Yovchev <plamenkooo@gmail.com>
2020-05-03 13:59:39 +0300

    Improve not found page compoent

--- reactivities-client/src/pages/NotFound.tsx
+++ reactivities-client/src/pages/NotFound.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-import { Segment } from "semantic-ui-react";
+import { Segment, Header, Icon, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <>
-      <Segment>Ooooops, we couldn't find that page!</Segment>
-    </>
+    <Segment placeholder>
+      <Header icon>
+        <Icon name="search" />
+        Oops - we've looked everywhere but couldn't find this.
+      </Header>
+      <Segment.Inline>
+        <Button as={Link} to="/activities" primary>
+          Return to Activities page
+        </Button>
+      </Segment.Inline>
+    </Segment>
   );
 };
 
