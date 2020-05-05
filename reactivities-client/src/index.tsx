716c1ea913813c0d72c3967cefbeabdd2b0c6f60
Plamen Yovchev <plamenkooo@gmail.com>
2020-05-05 21:00:56 +0300

    install semantic-ui-css

--- reactivities-client/src/index.tsx
+++ reactivities-client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import "react-toastify/dist/ReactToastify.min.css";
 import "react-widgets/dist/css/react-widgets.css";
+import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
