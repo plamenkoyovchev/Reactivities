297420e431494387ec895d5ab25984e516d8b47f
plamenyovchev <plamenkooo@gmail.com>
2020-05-07 23:16:33 +0300

    Add FbLogin component

---
+++ reactivities-client/src/components/User/FbLogin.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { Button, Icon } from "semantic-ui-react";
+
+interface IProps {
+  fbCallback: (response: any) => void;
+}
+
+const FbLogin: React.FC<IProps> = (fbCallback) => {
+  return (
+    <>
+      <FacebookLogin
+        appId="2744149992512877"
+        fields="name,email,picture"
+        callback={fbCallback}
+        render={(renderProps: any) => (
+          <Button
+            onClick={renderProps.onClick}
+            type="button"
+            fluid
+            color="facebook"
+          >
+            <Icon name="facebook" />
+            Login with Facebook
+          </Button>
+        )}
+      />
+    </>
+  );
+};
+
+export default FbLogin;
