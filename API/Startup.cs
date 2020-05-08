81e32045e55f8e217ada6b4ddb01d0e46b3e3330
plamenyovchev <plamenkooo@gmail.com>
2020-05-08 21:09:51 +0300

    Get FacebookAppSettings values from appsettings.json

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,6 +47,7 @@ namespace API
             services.AddScoped<IProfileReader, ProfileReader>();
 
             services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
+            services.Configure<FacebookAppSettings>(Configuration.GetSection("Authentication:Facebook"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
