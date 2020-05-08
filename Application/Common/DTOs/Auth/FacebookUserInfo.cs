a6fed60f9935da5721981d57c45c40c4142f3faa
plamenyovchev <plamenkooo@gmail.com>
2020-05-09 00:33:41 +0300

    Add Facebook data classes

--- Application/Common/DTOs/Auth/FacebookUserInfo.cs
+++ Application/Common/DTOs/Auth/FacebookUserInfo.cs
@@ -2,10 +2,12 @@ namespace Application.Common.DTOs.Auth
 {
     public class FacebookUserInfo
     {
+        public string Id { get; set; }
+
         public string Name { get; set; }
 
         public string Email { get; set; }
 
-        public string PictureUrl { get; set; }
+        public FacebookPictureInfo Picture { get; set; }
     }
 }
