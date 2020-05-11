ae901e995f8ee3e79679b90559a4e75dcbe26426
plamenyovchev <plamenkooo@gmail.com>
2020-05-11 22:12:04 +0300

    Implement FacebookLoginHandler

--- Application/Authentication/FacebookLogin/FacebookLoginQuery.cs
+++ Application/Authentication/FacebookLogin/FacebookLoginQuery.cs
@@ -1,9 +1,10 @@
 using Application.Common.DTOs.Auth;
+using Application.Common.ViewModels.User;
 using MediatR;
 
 namespace Application.Authentication.FacebookLogin
 {
-    public class FacebookLoginQuery : IRequest<FacebookUserInfo>
+    public class FacebookLoginQuery : IRequest<UserViewModel>
     {
         public string AccessToken { get; set; }
     }
