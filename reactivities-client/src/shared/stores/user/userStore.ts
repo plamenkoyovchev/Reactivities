912715eff29f18d8b8eac18531977f143fa26b2c
plamenyovchev <plamenkooo@gmail.com>
2020-05-11 22:20:47 +0300

    Implement fbLogin action

--- reactivities-client/src/shared/stores/user/userStore.ts
+++ reactivities-client/src/shared/stores/user/userStore.ts
@@ -1,4 +1,4 @@
-import { observable, action, computed } from "mobx";
+import { observable, action, computed, runInAction } from "mobx";
 import { IUser } from "../../../app/Models/User/IUser";
 import httpRequester from "../../axios/httpRequester";
 import { IUserFormValues } from "../../../app/Models/User/IUserFormValues";
@@ -51,7 +51,17 @@ class UserStore {
   };
 
   @action fbLogin = async (response: any) => {
-    console.log(response);
+    try {
+      const user = await httpRequester.user.fbLogin(response.accessToken);
+      runInAction(() => {
+        this.currentUser = user;
+      });
+      this.rootStore.commonStore.setToken(user.token);
+      this.rootStore.modalStore.close();
+      history.push("/activities");
+    } catch (error) {
+      toast.error("Login with Facebook failed!");
+    }
   };
 
   @action getCurrentUser = async () => {
