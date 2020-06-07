302f477438fc6ef04c3f58f802413059f9233479
plamenyovchev <plamenkooo@gmail.com>
2020-06-07 17:35:53 +0300

    Implement RefreshTokenHandler

--- Application/Authentication/RefreshToken/RefreshTokenHandler.cs
+++ Application/Authentication/RefreshToken/RefreshTokenHandler.cs
@@ -1,14 +1,46 @@
+using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Common.Constants.System;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.ViewModels.User;
+using AutoMapper;
+using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Authentication.RefreshToken
 {
-    public class RefreshTokenHandler : IRequestHandler<RefreshTokenQuery>
+    public class RefreshTokenHandler : IRequestHandler<RefreshTokenQuery, UserViewModel>
     {
-        public Task<Unit> Handle(RefreshTokenQuery request, CancellationToken cancellationToken)
+        private readonly UserManager<ReactivityUser> userManager;
+        private readonly IJwtGenerator jwtGenerator;
+        private readonly IMapper mapper;
+
+        public RefreshTokenHandler(UserManager<ReactivityUser> userManager, IJwtGenerator jwtGenerator, IMapper mapper)
         {
-            throw new System.NotImplementedException();
+            this.userManager = userManager;
+            this.jwtGenerator = jwtGenerator;
+            this.mapper = mapper;
+        }
+        public async Task<UserViewModel> Handle(RefreshTokenQuery request, CancellationToken cancellationToken)
+        {
+            var user = await this.userManager.FindByNameAsync(request.Username);
+            if (user == null || user.RefreshToken != request.RefreshToken || user.RefreshTokenExpiryDate < DateTime.Now)
+            {
+                throw new RestException(HttpStatusCode.Unauthorized);
+            }
+
+            user.RefreshToken = this.jwtGenerator.GenerateRefreshToken();
+            user.RefreshTokenExpiryDate = DateTime.Now.AddDays(ReactivitiesAppConstants.RefreshTokenExpiryInDays);
+            await this.userManager.UpdateAsync(user);
+
+            var userModel = this.mapper.Map<UserViewModel>(user);
+            userModel.Token = this.jwtGenerator.CreateToken(user);
+
+            return userModel;
         }
     }
 }
--- Application/Authentication/RefreshToken/RefreshTokenQuery.cs
+++ Application/Authentication/RefreshToken/RefreshTokenQuery.cs
@@ -1,9 +1,14 @@
+using Application.Common.ViewModels.User;
 using MediatR;
 
 namespace Application.Authentication.RefreshToken
 {
-    public class RefreshTokenQuery : IRequest
+    public class RefreshTokenQuery : IRequest<UserViewModel>
     {
+        public string Username { get; set; }
 
+        public string Token { get; set; }
+
+        public string RefreshToken { get; set; }
     }
 }
