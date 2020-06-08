7de59f4522b1d45caa1913d79f15fd0ba098b17f
Plamen Yovchev <plamenkooo@gmail.com>
2020-06-08 23:07:06 +0300

    Add reaction to save refreshToken to localStorage

--- reactivities-client/src/shared/stores/common/commonStore.ts
+++ reactivities-client/src/shared/stores/common/commonStore.ts
@@ -4,7 +4,7 @@ class CommonStore {
   constructor() {
     reaction(
       () => this.token,
-      token => {
+      (token) => {
         if (token) {
           localStorage.setItem("jwt", token);
         } else {
@@ -12,13 +12,31 @@ class CommonStore {
         }
       }
     );
+
+    reaction(
+      () => this.refreshToken,
+      (refreshToken) => {
+        if (refreshToken) {
+          localStorage.setItem("refreshToken", refreshToken);
+        } else {
+          localStorage.removeItem("refreshToken");
+        }
+      }
+    );
   }
 
   @observable token: string | null = localStorage.getItem("jwt");
+  @observable refreshToken: string | null = localStorage.getItem(
+    "refreshToken"
+  );
   @observable appLoaded: boolean = false;
 
   @action setToken = (token: string | null) => {
     this.token = token;
+  };
+
+  @action setRefreshToken = (refreshToken: string | null) => {
+    this.refreshToken = refreshToken;
   };
 
   @action setAppLoaded = () => {
